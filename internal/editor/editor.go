61d7d3399a456efcb491ea7a6a601055aa957b0d
hnnsb <hannes.b@wtnet.de>
2026-04-19 14:40:44 +0200

    refactor: pull key constants and keypress processing into input.go

--- internal/editor/editor.go
+++ internal/editor/editor.go
@@ -9,13 +9,10 @@ import (
 	"slices"
 	"strings"
 	"time"
-	"unicode/utf8"
 
 	"github.com/hnnsb/kigo/internal/ansi"
 	"golang.org/x/term"
 )
-
-/*** helper ***/
 
 // Config Constants
 const (
@@ -33,49 +30,6 @@ func getLineEnding() string {
 	}
 	return "\n"
 }
-
-// Key aliases
-const (
-	BACKSPACE  = 127 // ASCII backspace
-	ARROW_LEFT = iota + 1000
-	ARROW_RIGHT
-	ARROW_UP
-	ARROW_DOWN
-	DELETE_KEY
-	HOME_KEY
-	END_KEY
-	PAGE_UP
-	PAGE_DOWN
-	CTRL_ARROW_LEFT
-	CTRL_ARROW_RIGHT
-	CTRL_ARROW_UP
-	CTRL_ARROW_DOWN
-	CTRL_DELETE
-	CTRL_HOME
-	CTRL_END
-	CTRL_PAGE_UP
-	CTRL_PAGE_DOWN
-	SHIFT_ARROW_LEFT
-	SHIFT_ARROW_RIGHT
-	SHIFT_ARROW_UP
-	SHIFT_ARROW_DOWN
-	SHIFT_DELETE
-	SHIFT_HOME
-	SHIFT_END
-	SHIFT_PAGE_UP
-	SHIFT_PAGE_DOWN
-	ALT_ARROW_LEFT
-	ALT_ARROW_RIGHT
-	ALT_ARROW_UP
-	ALT_ARROW_DOWN
-	ALT_DELETE
-	ALT_HOME
-	ALT_END
-	ALT_PAGE_UP
-	ALT_PAGE_DOWN
-	CTRL_ALT_ARROW_UP
-	CTRL_ALT_ARROW_DOWN
-)
 
 // Syntax highlighting types
 const (
@@ -112,20 +66,7 @@ const (
 	HELP_MODE
 )
 
-// Check if the rune is a control character
-func isControl(r rune) bool {
-	return r < 32 || r == 127
-}
-
-// Check if the rune is a digit character
-func isDigit(r rune) bool {
-	return r >= '0' && r <= '9'
-}
-
-// Convert a character to its control key equivalent
-func withControlKey(c rune) rune {
-	return rune(int(c) & 0x1f) // 0x1f is 31 in decimal, which is the control character range
-}
+/*** helper ***/
 
 // runeIndexOf finds the index of the first occurrence of needle in haystack
 func runeIndexOf(haystack, needle []rune) int {
@@ -312,177 +253,6 @@ func (e *Editor) RestoreTerminal() {
 		term.Restore(int(os.Stdin.Fd()), e.terminal.originalState)
 		e.terminal.originalState = nil // Prevent multiple restoration attempts
 	}
-}
-
-func (e *Editor) readKey() (rune, error) {
-	buf := make([]byte, 1)
-	n, err := os.Stdin.Read(buf)
-
-	if n != 1 || err != nil {
-		return 0, errors.New("reading keyboard input")
-	}
-
-	c := buf[0]
-
-	// Handle escape sequences (special keys)
-	if c == ansi.ESC {
-		seq := make([]byte, 5)
-		if n, err := os.Stdin.Read(seq[0:2]); n != 2 || err != nil {
-			return ansi.ESC, nil // Return escape if we can't read sequence
-		}
-
-		switch seq[0] {
-		case '[':
-			if seq[1] >= '0' && seq[1] <= '9' {
-				if n, err := os.Stdin.Read(seq[2:3]); n != 1 || err != nil {
-					return ansi.ESC, nil
-				}
-				if seq[2] == '~' {
-					switch seq[1] {
-					case '1', '7':
-						return HOME_KEY, nil
-					case '3':
-						return DELETE_KEY, nil
-					case '4', '8':
-						return END_KEY, nil
-					case '5':
-						return PAGE_UP, nil
-					case '6':
-						return PAGE_DOWN, nil
-					}
-				}
-				if seq[2] == ';' { // Handle modifier keys (e.g., Shift, Ctrl)
-					if n, err := os.Stdin.Read(seq[3:5]); n != 2 || err != nil {
-						return ansi.ESC, nil
-					}
-					e.Debug("Modifier key sequence detected: %v - %s", seq, seq)
-					switch seq[3] {
-					case '2': // Shift
-						switch seq[4] {
-						case 'A':
-							return SHIFT_ARROW_UP, nil
-						case 'B':
-							return SHIFT_ARROW_DOWN, nil
-						case 'C':
-							return SHIFT_ARROW_RIGHT, nil
-						case 'D':
-							return SHIFT_ARROW_LEFT, nil
-						case 'H':
-							return SHIFT_HOME, nil
-						case 'F':
-							return SHIFT_END, nil
-						case '~':
-							switch seq[1] {
-							case '3':
-								return SHIFT_DELETE, nil
-							case '5':
-								return SHIFT_PAGE_UP, nil
-							case '6':
-								return SHIFT_PAGE_DOWN, nil
-							}
-						}
-					case '3': // Alt
-						return ansi.ESC, nil // For now, we will not handle alt+arrows differently
-					case '5': // Ctrl
-						switch seq[4] {
-						case 'A':
-							return CTRL_ARROW_UP, nil
-						case 'B':
-							return CTRL_ARROW_DOWN, nil
-						case 'C':
-							return CTRL_ARROW_RIGHT, nil
-						case 'D':
-							return CTRL_ARROW_LEFT, nil
-						case 'H':
-							return CTRL_HOME, nil
-						case 'F':
-							return CTRL_END, nil
-						case '~':
-							switch seq[1] {
-							case '3':
-								return CTRL_DELETE, nil
-							case '5':
-								return CTRL_PAGE_UP, nil
-							case '6':
-								return CTRL_PAGE_DOWN, nil
-							}
-						}
-					case '6': // Ctrl+Shift
-						return ansi.ESC, nil
-					case '7': // Ctrl+Alt
-						switch seq[4] {
-						case 'A':
-							return CTRL_ALT_ARROW_UP, nil
-						case 'B':
-							return CTRL_ALT_ARROW_DOWN, nil
-						}
-					}
-				}
-			} else {
-				switch seq[1] {
-				case 'A':
-					return ARROW_UP, nil
-				case 'B':
-					return ARROW_DOWN, nil
-				case 'C':
-					return ARROW_RIGHT, nil
-				case 'D':
-					return ARROW_LEFT, nil
-				case 'H':
-					return HOME_KEY, nil
-				case 'F':
-					return END_KEY, nil
-				}
-			}
-		case 'O':
-			switch seq[1] {
-			case 'H':
-				return HOME_KEY, nil
-			case 'F':
-				return END_KEY, nil
-			}
-		}
-		return ansi.ESC, nil // Unknown escape sequence, return escape
-	}
-
-	// For ASCII characters, return directly
-	if c < 128 {
-		e.Debug("Read character %v - %c", c, c)
-		return rune(c), nil
-	}
-
-	// Handle multi-byte UTF-8 characters
-	// Put the first byte back and read the full UTF-8 sequence
-	var utfBuf [4]byte
-	utfBuf[0] = c
-
-	// Determine how many more bytes we need
-	var totalBytes int
-	if c&0xE0 == 0xC0 {
-		totalBytes = 2
-	} else if c&0xF0 == 0xE0 {
-		totalBytes = 3
-	} else if c&0xF8 == 0xF0 {
-		totalBytes = 4
-	} else {
-		return utf8.RuneError, errors.New("invalid UTF-8 sequence")
-	}
-
-	// Read remaining bytes
-	if totalBytes > 1 {
-		n, err := os.Stdin.Read(utfBuf[1:totalBytes])
-		if n != totalBytes-1 || err != nil {
-			return utf8.RuneError, errors.New("reading UTF-8 sequence")
-		}
-	}
-
-	// Decode UTF-8
-	r, size := utf8.DecodeRune(utfBuf[:totalBytes])
-	if r == utf8.RuneError || size != totalBytes {
-		return utf8.RuneError, errors.New("invalid UTF-8 character")
-	}
-
-	return r, nil
 }
 
 func getWindowsSize() (int, int, error) {
--- internal/editor/input.go
+++ internal/editor/input.go
@@ -1,12 +1,252 @@
 package editor
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"slices"
+	"unicode/utf8"
+
+	"github.com/hnnsb/kigo/internal/ansi"
 )
 
 var quitTimes = QUIT_TIMES
+
+// Key aliases
+const (
+	BACKSPACE  = 127 // ASCII backspace
+	ARROW_LEFT = iota + 1000
+	ARROW_RIGHT
+	ARROW_UP
+	ARROW_DOWN
+	DELETE_KEY
+	HOME_KEY
+	END_KEY
+	PAGE_UP
+	PAGE_DOWN
+	CTRL_ARROW_LEFT
+	CTRL_ARROW_RIGHT
+	CTRL_ARROW_UP
+	CTRL_ARROW_DOWN
+	CTRL_DELETE
+	CTRL_HOME
+	CTRL_END
+	CTRL_PAGE_UP
+	CTRL_PAGE_DOWN
+	SHIFT_ARROW_LEFT
+	SHIFT_ARROW_RIGHT
+	SHIFT_ARROW_UP
+	SHIFT_ARROW_DOWN
+	SHIFT_DELETE
+	SHIFT_HOME
+	SHIFT_END
+	SHIFT_PAGE_UP
+	SHIFT_PAGE_DOWN
+	ALT_ARROW_LEFT
+	ALT_ARROW_RIGHT
+	ALT_ARROW_UP
+	ALT_ARROW_DOWN
+	ALT_DELETE
+	ALT_HOME
+	ALT_END
+	ALT_PAGE_UP
+	ALT_PAGE_DOWN
+	CTRL_ALT_ARROW_UP
+	CTRL_ALT_ARROW_DOWN
+)
+
+/*** helper ***/
+
+// Convert a character to its control key equivalent
+func withControlKey(c rune) rune {
+	return rune(int(c) & 0x1f) // 0x1f is 31 in decimal, which is the control character range
+}
+
+// Check if the rune is a control character
+func isControl(r rune) bool {
+	return r < 32 || r == 127
+}
+
+// Check if the rune is a digit character
+func isDigit(r rune) bool {
+	return r >= '0' && r <= '9'
+}
+
+/*** input ***/
+
+// readKey reads then input sequence from Stdin and returns the corresponding
+// rune or constant alias for control sequences
+func (e *Editor) readKey() (rune, error) {
+	buf := make([]byte, 1)
+	n, err := os.Stdin.Read(buf)
+
+	if n != 1 || err != nil {
+		return 0, errors.New("reading keyboard input")
+	}
+
+	c := buf[0]
+
+	// Handle escape sequences (special keys)
+	if c == ansi.ESC {
+		seq := make([]byte, 5)
+		if n, err := os.Stdin.Read(seq[0:2]); n != 2 || err != nil {
+			return ansi.ESC, nil // Return escape if we can't read sequence
+		}
+
+		switch seq[0] {
+		case '[':
+			if seq[1] >= '0' && seq[1] <= '9' {
+				if n, err := os.Stdin.Read(seq[2:3]); n != 1 || err != nil {
+					return ansi.ESC, nil
+				}
+				if seq[2] == '~' {
+					switch seq[1] {
+					case '1', '7':
+						return HOME_KEY, nil
+					case '3':
+						return DELETE_KEY, nil
+					case '4', '8':
+						return END_KEY, nil
+					case '5':
+						return PAGE_UP, nil
+					case '6':
+						return PAGE_DOWN, nil
+					}
+				}
+				if seq[2] == ';' { // Handle modifier keys (e.g., Shift, Ctrl)
+					if n, err := os.Stdin.Read(seq[3:5]); n != 2 || err != nil {
+						return ansi.ESC, nil
+					}
+					e.Debug("Modifier key sequence detected: %v - %s", seq, seq)
+					switch seq[3] {
+					case '2': // Shift
+						switch seq[4] {
+						case 'A':
+							return SHIFT_ARROW_UP, nil
+						case 'B':
+							return SHIFT_ARROW_DOWN, nil
+						case 'C':
+							return SHIFT_ARROW_RIGHT, nil
+						case 'D':
+							return SHIFT_ARROW_LEFT, nil
+						case 'H':
+							return SHIFT_HOME, nil
+						case 'F':
+							return SHIFT_END, nil
+						case '~':
+							switch seq[1] {
+							case '3':
+								return SHIFT_DELETE, nil
+							case '5':
+								return SHIFT_PAGE_UP, nil
+							case '6':
+								return SHIFT_PAGE_DOWN, nil
+							}
+						}
+					case '3': // Alt
+						// Alt+arrows are used in Windows Terminal to switch between split windows, so sequnces are not send to kigo.
+						return ansi.ESC, nil // For now, we will not handle alt+arrows differently
+					case '5': // Ctrl
+						switch seq[4] {
+						case 'A':
+							return CTRL_ARROW_UP, nil
+						case 'B':
+							return CTRL_ARROW_DOWN, nil
+						case 'C':
+							return CTRL_ARROW_RIGHT, nil
+						case 'D':
+							return CTRL_ARROW_LEFT, nil
+						case 'H':
+							return CTRL_HOME, nil
+						case 'F':
+							return CTRL_END, nil
+						case '~':
+							switch seq[1] {
+							case '3':
+								return CTRL_DELETE, nil
+							case '5':
+								return CTRL_PAGE_UP, nil
+							case '6':
+								return CTRL_PAGE_DOWN, nil
+							}
+						}
+					case '6': // Ctrl+Shift
+						return ansi.ESC, nil
+					case '7': // Ctrl+Alt
+						switch seq[4] {
+						case 'A':
+							return CTRL_ALT_ARROW_UP, nil
+						case 'B':
+							return CTRL_ALT_ARROW_DOWN, nil
+						}
+					}
+				}
+			} else {
+				switch seq[1] {
+				case 'A':
+					return ARROW_UP, nil
+				case 'B':
+					return ARROW_DOWN, nil
+				case 'C':
+					return ARROW_RIGHT, nil
+				case 'D':
+					return ARROW_LEFT, nil
+				case 'H':
+					return HOME_KEY, nil
+				case 'F':
+					return END_KEY, nil
+				}
+			}
+		case 'O':
+			switch seq[1] {
+			case 'H':
+				return HOME_KEY, nil
+			case 'F':
+				return END_KEY, nil
+			}
+		}
+		return ansi.ESC, nil // Unknown escape sequence, return escape
+	}
+
+	// For ASCII characters, return directly
+	if c < 128 {
+		e.Debug("Read character %v - %c", c, c)
+		return rune(c), nil
+	}
+
+	// Handle multi-byte UTF-8 characters
+	// Put the first byte back and read the full UTF-8 sequence
+	var utfBuf [4]byte
+	utfBuf[0] = c
+
+	// Determine how many more bytes we need
+	var totalBytes int
+	if c&0xE0 == 0xC0 {
+		totalBytes = 2
+	} else if c&0xF0 == 0xE0 {
+		totalBytes = 3
+	} else if c&0xF8 == 0xF0 {
+		totalBytes = 4
+	} else {
+		return utf8.RuneError, errors.New("invalid UTF-8 sequence")
+	}
+
+	// Read remaining bytes
+	if totalBytes > 1 {
+		n, err := os.Stdin.Read(utfBuf[1:totalBytes])
+		if n != totalBytes-1 || err != nil {
+			return utf8.RuneError, errors.New("reading UTF-8 sequence")
+		}
+	}
+
+	// Decode UTF-8
+	r, size := utf8.DecodeRune(utfBuf[:totalBytes])
+	if r == utf8.RuneError || size != totalBytes {
+		return utf8.RuneError, errors.New("invalid UTF-8 character")
+	}
+
+	return r, nil
+}
 
 func (e *Editor) Prompt(prompt string, callback func([]byte, int)) string {
 	bufSize := 128
@@ -32,7 +272,7 @@ func (e *Editor) Prompt(prompt string, callback func([]byte, int)) string {
 				callback(buf, int(key))
 			}
 
-		case '\x1b': // Escape
+		case ansi.ESC: // Escape
 			e.SetStatusMessage("")
 			if callback != nil {
 				callback(buf, int(key))
@@ -72,6 +312,7 @@ func (e *Editor) Prompt(prompt string, callback func([]byte, int)) string {
 	}
 }
 
+// ProcessKeypress reads an input sequence and initiates the correct editor action
 func (e *Editor) ProcessKeypress() {
 	key, err := e.readKey()
 	if err != nil {
@@ -244,15 +485,15 @@ func (e *Editor) handleAutoPairKey(key rune) bool {
 	case '(':
 		e.insertPair('(', ')')
 	case ')':
-		e.skipOrInsertClosing(')')
+		e.skipOrInsert(')')
 	case '{':
 		e.insertPair('{', '}')
 	case '}':
-		e.skipOrInsertClosing('}')
+		e.skipOrInsert('}')
 	case '[':
 		e.insertPair('[', ']')
 	case ']':
-		e.skipOrInsertClosing(']')
+		e.skipOrInsert(']')
 	case '"':
 		e.insertPair('"', '"')
 	case '\'':
@@ -291,29 +532,32 @@ func (e *Editor) handleEnterKey() {
 		}
 	}
 
+	// Add extra newline and indent if inside brackets
 	if e.syntax != nil && e.syntax.cfgFlags&CFG_ADD_NEWLINE_ON_PARENTHESES != 0 && indentLine {
 		e.InsertNewline()
 		e.MoveCursor(ARROW_LEFT)
 		for range e.syntax.indentationSize {
 			e.InsertRune(e.syntax.indentationChar)
-			e.RefreshScreen()
 		}
 	}
 }
 
+// insertPair inserts the rune pair and positions the cursor inside the pair.
 func (e *Editor) insertPair(open rune, close rune) {
 	e.InsertRune(open)
 	e.InsertRune(close)
 	e.MoveCursor(ARROW_LEFT)
 }
 
-func (e *Editor) skipOrInsertClosing(close rune) {
+// skipOrInsert inserts the given rune r, only if the next character is not
+// the same.
+func (e *Editor) skipOrInsert(r rune) {
 	if e.cy < e.totalRows {
 		row := &e.rows[e.cy]
-		if e.cx < len(row.chars) && row.chars[e.cx] == close {
+		if e.cx < len(row.chars) && row.chars[e.cx] == r {
 			e.MoveCursor(ARROW_RIGHT)
 			return
 		}
 	}
-	e.InsertRune(close)
+	e.InsertRune(r)
 }
